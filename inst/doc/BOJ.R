805f7689f865ef720260298870f98ee7e227ee60
Stefan Angrick <contact@stefanangrick.net>
2021-02-23 05:30:02 +0000

    version 0.2.3

--- inst/doc/BOJ.R
+++ inst/doc/BOJ.R
@@ -14,23 +14,25 @@ library("BOJ")
 datasets <- get_boj_datasets()
 datasets
 
-## ----bop, message=FALSE, warning=FALSE----------------------------------------
-bop <- get_boj(datasets$url[(datasets$name == "bp_m_en")])
-bop
+## ----sppi, message=FALSE, warning=FALSE---------------------------------------
+sppi <- get_boj(datasets$url[(datasets$name == "sppi_m_en")])
+sppi
 
 ## ----plot, message=FALSE, warning=FALSE---------------------------------------
 library("dplyr")
 library("ggplot2")
 library("zoo")
 
-bop_plot <- subset(bop, code %in% c("BPBP6JYNTB", "BPBP6JYNSN", "BPBP6JYNPIN",
-                                    "BPBP6JYNSIN"))
-bop_plot <- mutate(bop_plot, date = as.Date(as.yearmon(date, format = "%Y%m")))
-bop_plot <- subset(bop_plot, date > as.Date("2000-01-01"))
-bop_plot <- subset(bop_plot, !is.na(obs_value))
+sppi_plot <- subset(sppi, code %in% c("PRCS15_5200000000", "PRCS15_5200010001",
+                                      "PRCS15_5200010002", "PRCS15_5200010003",
+                                      "PRCS15_5200010004", "PRCS15_5200010005",
+                                      "PRCS15_5200010006", "PRCS15_5200010007"))
+sppi_plot <- mutate(sppi_plot, date = as.Date(as.yearmon(date, format = "%Y%m")))
+sppi_plot <- mutate(sppi_plot, struc = gsub("^Major group/ ", "", struc))
+sppi_plot <- subset(sppi_plot, !is.na(obs_value))
 
-ggplot(bop_plot, aes(x = date, y = obs_value)) +
-  geom_bar(aes(fill = struc), stat = "identity") +
-  labs(x = "Date", y = "100 million yen") +
+ggplot(sppi_plot, aes(x = date, y = obs_value)) +
+  geom_line(aes(colour = struc)) +
+  labs(x = "Date", y = "Services Producer Price Index (2015 base)") +
   theme(legend.title = element_blank())
 
