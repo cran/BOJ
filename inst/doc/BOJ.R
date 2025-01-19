162509f39790ca55e7372823793749eca94242bd
Stefan Angrick <contact@stefanangrick.net>
2025-01-19 11:10:02 +0000

    version 0.3.4

--- inst/doc/BOJ.R
+++ inst/doc/BOJ.R
@@ -1,38 +1,34 @@
-## ----setup, echo = FALSE------------------------------------------------------
-knitr::opts_chunk$set(
-  message = FALSE,
-  warning = FALSE,
-  collapse = TRUE,
-  fig.width = 6, 
-  fig.height = 4
-)
+## ----install, eval=FALSE, message=FALSE, warning=FALSE------------------------
+# library(devtools)
+# install_github("stefanangrick/BOJ")  # GitHub
+# install.packages("BOJ")              # CRAN
 
-## ----loading------------------------------------------------------------------
-library("BOJ")
+## ----loading, eval=FALSE, message=FALSE, warning=FALSE------------------------
+# library("BOJ")
 
-## ----datasets, message=FALSE, warning=FALSE-----------------------------------
-datasets <- get_boj_datasets()
-datasets
+## ----datasets, eval=FALSE, message=FALSE, warning=FALSE-----------------------
+# ds <- get_boj_datasets()
+# head(ds)
 
-## ----sppi, message=FALSE, warning=FALSE---------------------------------------
-sppi <- get_boj(datasets$url[(datasets$name == "sppi_m_en")])
-sppi
+## ----sppi, eval=FALSE, message=FALSE, warning=FALSE---------------------------
+# sppi <- get_boj(ds$url[(ds$name == "sppi_m_en")])
+# head(sppi)
 
-## ----plot, message=FALSE, warning=FALSE---------------------------------------
-library("dplyr")
-library("ggplot2")
-library("zoo")
-
-sppi_plot <- subset(sppi, code %in% c("PRCS15_5200000000", "PRCS15_5200010001",
-                                      "PRCS15_5200010002", "PRCS15_5200010003",
-                                      "PRCS15_5200010004", "PRCS15_5200010005",
-                                      "PRCS15_5200010006", "PRCS15_5200010007"))
-sppi_plot <- mutate(sppi_plot, date = as.Date(as.yearmon(date, format = "%Y%m")))
-sppi_plot <- mutate(sppi_plot, struc = gsub("^Major group/ ", "", struc))
-sppi_plot <- subset(sppi_plot, !is.na(obs_value))
-
-ggplot(sppi_plot, aes(x = date, y = obs_value)) +
-  geom_line(aes(colour = struc)) +
-  labs(x = "Date", y = "Services Producer Price Index (2015 base)") +
-  theme(legend.title = element_blank())
+## ----plot, eval=FALSE, message=FALSE, warning=FALSE---------------------------
+# library("dplyr")
+# library("ggplot2")
+# library("zoo")
+# 
+# sppi_plot <- subset(sppi, code %in% c("PRCS20_5200000000", "PRCS20_5200010001",
+#                                       "PRCS20_5200010002", "PRCS20_5200010003",
+#                                       "PRCS20_5200010004", "PRCS20_5200010005",
+#                                       "PRCS20_5200010006", "PRCS20_5200010007"))
+# sppi_plot <- mutate(sppi_plot, date = as.Date(as.yearmon(date, format = "%Y%m")))
+# sppi_plot <- mutate(sppi_plot, struc = gsub("^Major group/ ", "", struc))
+# sppi_plot <- subset(sppi_plot, !is.na(obs_value))
+# 
+# ggplot(sppi_plot, aes(x = date, y = obs_value)) +
+#   geom_line(aes(colour = struc)) +
+#   labs(title = "Services Producer Price Index", x = "Date", y = "Index") +
+#   theme(legend.title = element_blank())
 
